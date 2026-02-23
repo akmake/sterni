5d792ad36b37ccca5cae65b1f63586d47d555ce2
akmake <92978132+akmake@users.noreply.github.com>
2026-02-23 14:11:51 +0200

    22

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      includeAssets: ['opo.png', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'ניהול קבוצות ואירועים',
         short_name: 'ניהול',
